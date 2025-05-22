6fc6ac56190eee87d4c1d5ff45fb180ffb189379
Victor Bello <stywan.v@gmail.com>
2025-05-22 01:51:52 -0400

    feat: se implementa el método de registro en UserService

--- src/main/java/com/ecomarket/auth_service/service/UserService.java
+++ src/main/java/com/ecomarket/auth_service/service/UserService.java
@@ -1,4 +1,40 @@
 package com.ecomarket.auth_service.service;
 
+import com.ecomarket.auth_service.DTO.UserDTO;
+import com.ecomarket.auth_service.DTO.UserRegisterRequest;
+import com.ecomarket.auth_service.exception.EmailAlreadyExistsException;
+import com.ecomarket.auth_service.model.User;
+import com.ecomarket.auth_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserDTO registerUser(UserRegisterRequest request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new EmailAlreadyExistsException(request.getEmail());
+        }
+
+        User user = User.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .fullName(request.getFullName())
+                .role(User.Role.CLIENT)
+                .build();
+
+        userRepository.save(user);
+
+        return UserDTO.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .fullName(user.getFullName())
+                .role(user.getRole())
+                .build();
+    }
 }
