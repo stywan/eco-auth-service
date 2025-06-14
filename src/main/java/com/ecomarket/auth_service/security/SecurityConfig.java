d5ccd4ea88d6243922867d09c97bc67e59e74ae7
Victor Bello <stywan.v@gmail.com>
2025-06-14 18:39:12 -0400

    implementacion userloginRequest/userController/segurityConfig

--- src/main/java/com/ecomarket/auth_service/security/SecurityConfig.java
+++ src/main/java/com/ecomarket/auth_service/security/SecurityConfig.java
@@ -2,12 +2,28 @@ package com.ecomarket.auth_service.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/register", "/api/v1/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(httpBasic -> httpBasic
+                        .realmName("Ecomarket Auth Realm")
+                );
+
+        return http.build();
     }
 }
